48b4f7e8ffd33f9cda1a50fa5c8d612dd16469fd
philip nguyen <nguyen.is.philip@gmail.com>
2024-01-29 19:11:14 -0500

    added get routes to server.js

--- server.js
+++ server.js
@@ -9,4 +9,18 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/api/notes', (req, res) => {
+    res.json(allNotes.slice(1));
+});
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
