73abc9100947708f980480d6e0f703a2b34714af
philip nguyen <nguyen.is.philip@gmail.com>
2024-01-29 19:03:16 -0500

    added server.js and began working on it

---
+++ server.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+const db = require('./db/db.json')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
